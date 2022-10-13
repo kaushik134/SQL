7fdafbacf0a7205ba84a39300d7f47aff7ced85f
kaushik134 <kaushikvaghasiya134@gmail.com>
2022-10-13 19:36:19 +0530

    commit foreign

--- DB_SKillQode/sales_order.sql
+++ DB_SKillQode/sales_order.sql
@@ -1,14 +1,25 @@
--- SALES_ORDER TABLE
-create table sales_order(
-    orderno varchar(30) primary key,
-    Clientno varchar(30),
-    orderdate date,
-    delyaddr varchar(25),
-    salesman_no varchar(6),
-    delytype char(1),
-    billyn char(1),
-    delydate date,
-    orderstatus varchar(10),
-    CONSTRAINT fk_salesman FOREIGN KEY(salesman_no) REFERENCES salesman_master(salesman_no),
-    CONSTRAINT fk_client FOREIGN KEY(Clientno) REFERENCES client_master(Clientno)
-);
+-- -- SALES_ORDER TABLE
+-- create table sales_order(
+--     orderno varchar(30) primary key,
+--     Clientno varchar(30),
+--     orderdate date,
+--     delyaddr varchar(25),
+--     salesman_no varchar(6),
+--     delytype char(1),
+--     billyn char(1),
+--     delydate date,
+--     orderstatus varchar(10),
+--     CONSTRAINT fk_salesman FOREIGN KEY(salesman_no) REFERENCES salesman_master(salesman_no),
+--     CONSTRAINT fk_client FOREIGN KEY(Clientno) REFERENCES client_master(Clientno)
+-- );
+
+-- insert record
+insert into sales_order values('O19001','C00001','2004-04-12','Bombay','S00001','F','N','2002-02-20','In Process');
+insert into sales_order values('O19002','C00002','2004-04-25','Madras','S00002','P','N','2002-02-27','Cancelled');
+insert into sales_order values('O19003','C00003','2004-04-18','Bombay','S00003','F','Y','2002-02-20','Fulfilled');
+insert into sales_order values('O19004','C00004','2004-04-03','Bombay','S00004','F','Y','2002-02-07','Fulfilled');
+insert into sales_order values('O19005','C00005','2004-04-20','Delhi','S00002','P','N','2002-02-22','Canceleed');
+insert into sales_order values('O19006','C00006','2004-04-24','Bombay','S00003','F','N','2002-02-26','In Process');
+
+-- Show Tables
+select * from sales_order;
--- DB_SKillQode/sales_order_detail.sql
+++ DB_SKillQode/sales_order_detail.sql
@@ -8,3 +8,23 @@ create table sod(
     CONSTRAINT fk_Sales_order FOREIGN KEY (orderno) REFERENCES sales_order (orderno),
     CONSTRAINT fk_Product_master FOREIGN KEY (productno) REFERENCES product_master (productno)
 );
+
+-- Insert record
+insert into sod values('O19001','P00001',4,4,525);
+insert into sod values('O19001','P00002',2,1,8400);
+insert into sod values('O19001','P00002',2,1,5250);
+insert into sod values('O19002','P00003',10,0,525);
+insert into sod values('O19005','P00001',3,3,3150);
+insert into sod values('O19004','P00003',3,1,5250);
+insert into sod values('O19005','P00004',10,10,525);
+insert into sod values('O19005','P00005',4,4,1050);
+insert into sod values('O19006','P00003',2,2,1050);
+insert into sod values('O19003','P00004',1,1,12000);
+insert into sod values('O19003','P00005',1,0,8400);
+insert into sod values('O19006','P00006',1,0,1050);
+insert into sod values('O19006','P00006',10,5,525);
+insert into sod values('O19002','P00007',5,3,1050);
+insert into sod values('O19002','P00008',5,3,1050);
+
+-- Show Tables
+select * from sod;
