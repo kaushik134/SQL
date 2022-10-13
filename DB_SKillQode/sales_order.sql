dc1e81ba50f54056b29380beea8adff32d4d7b76
kaushik134 <kaushikvaghasiya134@gmail.com>
2022-10-13 19:46:15 +0530

    commit

--- DB_SKillQode/sales_order.sql
+++ DB_SKillQode/sales_order.sql
@@ -1,17 +1,17 @@
 -- -- SALES_ORDER TABLE
--- create table sales_order(
---     orderno varchar(30) primary key,
---     Clientno varchar(30),
---     orderdate date,
---     delyaddr varchar(25),
---     salesman_no varchar(6),
---     delytype char(1),
---     billyn char(1),
---     delydate date,
---     orderstatus varchar(10),
---     CONSTRAINT fk_salesman FOREIGN KEY(salesman_no) REFERENCES salesman_master(salesman_no),
---     CONSTRAINT fk_client FOREIGN KEY(Clientno) REFERENCES client_master(Clientno)
--- );
+create table sales_order(
+    orderno varchar(30) primary key,
+    Clientno varchar(30),
+    orderdate date,
+    delyaddr varchar(25),
+    salesman_no varchar(6),
+    delytype char(1),
+    billyn char(1),
+    delydate date,
+    orderstatus varchar(10),
+    CONSTRAINT fk_salesman FOREIGN KEY(salesman_no) REFERENCES salesman_master(salesman_no),
+    CONSTRAINT fk_client FOREIGN KEY(Clientno) REFERENCES client_master(Clientno)
+);
 
 -- insert record
 insert into sales_order values('O19001','C00001','2004-04-12','Bombay','S00001','F','N','2002-02-20','In Process');
